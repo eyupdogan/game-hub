2ba58c02d6ab37507a8e0d14a2e7c56a1bccfb08
Eyup Dogan <eyodohan@gmail.com>
2024-03-16 22:29:25 +0300

    implementing infinite scroll

--- src/components/GameGrid.tsx
+++ src/components/GameGrid.tsx
@@ -1,10 +1,11 @@
-import { Box, Button, SimpleGrid, Text } from '@chakra-ui/react';
+import { SimpleGrid, Spinner, Text } from '@chakra-ui/react';
 import useGames from '../hooks/useGames';
 import GameCard from './GameCard';
 import GameCardSkeleton from './GameCardSkeleton';
 import GameCardContainer from './GameCardContainer';
 import { GameQuery } from '../App';
 import React from 'react';
+import InfiniteScroll from 'react-infinite-scroll-component';
 
 interface Props {
   gameQuery: GameQuery;
@@ -16,9 +17,11 @@ const GameGrid = ({gameQuery}: Props) => {
 
     if (error) return <Text>{error.message}</Text>
 
+    const fetchGamesCount = data?.pages.reduce((total, page) => total + page.results.length, 0) || 0
+
   return (
-        <Box padding={10}>
-          <SimpleGrid columns={{sm: 1, md: 2, lg: 3, xl: 4}} spacing={6}>
+          <InfiniteScroll dataLength={fetchGamesCount} hasMore={!!hasNextPage} next={() => fetchNextPage()} loader={<Spinner />}>
+          <SimpleGrid columns={{sm: 1, md: 2, lg: 3, xl: 4}} spacing={6} padding={10}>
               {isLoading && skeletons.map(skeleton => <GameCardSkeleton key={skeleton} />)}
               {data?.pages.map((games, index) => <React.Fragment key={index}>
                 {games.results.map(game => <GameCardContainer key={game.id}>
@@ -26,8 +29,7 @@ const GameGrid = ({gameQuery}: Props) => {
               </GameCardContainer>)}
               </React.Fragment>)}
           </SimpleGrid>
-          {hasNextPage && <Button marginY={5} disabled={isFetchingNextPage} onClick={() =>fetchNextPage()}>{isFetchingNextPage ? "Loading..." : "Load More"}</Button>}
-        </Box>
+          </InfiniteScroll>
   )
 }
 
--- src/hooks/useGames.ts
+++ src/hooks/useGames.ts
@@ -28,7 +28,8 @@ const useGames = (gameQuery: GameQuery) => {
         keepPreviousData: true,
         getNextPageParam: (lastPage, allPages) => {
             return lastPage.next ? allPages.length + 1 : undefined
-        }
+        },
+        staleTime: 24 * 60 * 60 * 1000 //24h
     })
 }
 
