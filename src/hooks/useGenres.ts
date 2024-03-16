001f065c34e0e8bd0ca041762d65097b8b684100
Eyup Dogan <eyodohan@gmail.com>
2024-03-16 23:03:38 +0300

    creating lookup hooks

--- src/hooks/useGenres.ts
+++ src/hooks/useGenres.ts
@@ -14,7 +14,7 @@ const useGenres = () => {
         queryKey: ["genres"],
         queryFn: apiClient.get,
         staleTime: 24 * 60 * 60 * 1000,
-        initialData: {results: genres, count: genres.length}
+        initialData: genres
     })
 }
 
--- src/hooks/usePlatforms.ts
+++ src/hooks/usePlatforms.ts
@@ -13,7 +13,7 @@ const usePlatforms = () => useQuery({
     queryKey: ["platforms"],
     queryFn: apiClient.get,
     staleTime: 24 * 60 * 60 * 1000,
-    initialData: {count: platforms.length, results: platforms}
+    initialData: platforms
 })
 
 
